e8a087d7c77b38ed73db87c886acb8127eba45bb
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2016-08-17 16:52:24 -0300

    Refactoring MainActivity

--- app/src/main/java/com/brayanbedritchuk/zerotohero/main/MainActivity.java
+++ app/src/main/java/com/brayanbedritchuk/zerotohero/main/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.brayanbedritchuk.zerotohero.R;
@@ -15,71 +14,78 @@ import com.brayanbedritchuk.zerotohero.workout_list.view.WorkoutListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int WORKOUT_LIST_FRAGMENT_POSITION = 0;
-    private static final int EXERCISE_LIST_FRAGMENT_POSITION = 1;
-
-    private ViewPager viewPager;
-    private MainFragmentPagerAdapter pagerAdapter;
+    private static final int WORKOUT_LIST_POSITION = 0;
+    private static final int EXERCISE_LIST_POSITION = 1;
 
     private Toolbar toolbar;
-
-    private TextView tvToolbarTitle;
-    private TabLayout tab;
-
-    private FloatingActionButton btNewWorkout;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private MainFragmentPagerAdapter pagerAdapter;
+    private FloatingActionButton fab;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-        tab = (TabLayout) findViewById(R.id.tabs);
-
-        viewPager = (ViewPager) findViewById(R.id.pager);
-
-        pagerAdapter =
-                new MainFragmentPagerAdapter(
-                        getSupportFragmentManager());
-        viewPager.setAdapter(pagerAdapter);
-
-        tab.setupWithViewPager(viewPager);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        tvToolbarTitle = (TextView) findViewById(R.id.toolbar_title);
-
-        btNewWorkout = (FloatingActionButton) findViewById(R.id.fragment_workout_list__bt__new_workout);
-
-        btNewWorkout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int currentPosition = tab.getSelectedTabPosition();
-
-                switch (currentPosition) {
-                    case WORKOUT_LIST_FRAGMENT_POSITION:
-                        ((WorkoutListFragment) pagerAdapter.getRegisteredFragment(WORKOUT_LIST_FRAGMENT_POSITION)).onClickFab();
-                        return;
-
-                    case EXERCISE_LIST_FRAGMENT_POSITION:
-                        Toast.makeText(MainActivity.this, "Nothing yet", Toast.LENGTH_SHORT).show();
-                        return;
-                }
-            }
-        });
-
+    private void initViews() {
+        inflateViews();
         initToolbar();
+        initViewPager();
+        initListeners();
+    }
+
+    private void inflateViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        fab = (FloatingActionButton) findViewById(R.id.activity_main_fab);
     }
 
     private void initToolbar() {
-        initAppCompatActivity();
-        initToolbarTitle();
-    }
-
-    protected void initAppCompatActivity() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
-    private void initToolbarTitle() {
-        tvToolbarTitle.setText("Zero to Hero!");
+    private void initViewPager() {
+        pagerAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    private void initListeners() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickFab();
+            }
+        });
+    }
+
+    private void onClickFab() {
+
+        switch (getCurrentTabPosition()) {
+
+            case WORKOUT_LIST_POSITION: {
+                getWorkoutListFragment().onClickFab();
+                return;
+            }
+
+            case EXERCISE_LIST_POSITION: {
+                Toast.makeText(MainActivity.this, "Nothing yet", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    }
+
+    private WorkoutListFragment getWorkoutListFragment() {
+        return ((WorkoutListFragment) pagerAdapter.getRegisteredFragment(WORKOUT_LIST_POSITION));
+    }
+
+    private int getCurrentTabPosition() {
+        return tabLayout.getSelectedTabPosition();
+    }
+
 }
