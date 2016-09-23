aa0a1ba2bcada03e12ed7f4e218ea9e91261c8c6
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2016-09-23 11:39:26 -0300

    Improve simplicity (1 Sprint)

--- app/src/main/java/com/brayanbedritchuk/zerotohero/helper/DecimalHelper.java
+++ app/src/main/java/com/brayanbedritchuk/zerotohero/helper/DecimalHelper.java
@@ -1,14 +1,12 @@
 package com.brayanbedritchuk.zerotohero.helper;
 
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 
 public class DecimalHelper {
 
+    private static final String PATTERN_REAIS = "'R$'###.###.###.###.###,##";
+
     public static String getReais(double valor) {
-        double d1 = 1.234567;
-        double d2 = 2;
-        NumberFormat nf = new DecimalFormat("'R$'#.##,##");
-        return nf.format(d2);
+        return new DecimalFormat(PATTERN_REAIS).format(valor);
     }
 }
