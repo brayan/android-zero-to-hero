59a6855dfb4939138aaac2f2eb8a226e01b5c68f
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-06-29 10:17:12 -0300

    Remove old Zero to Hero android structure

--- app/src/main/java/br/com/sailboat/zerotohero/view/adapter/ItemTouchHelperAdapter.java
+++
@@ -1,9 +0,0 @@
-package br.com.sailboat.zerotohero.view.adapter;
-
-public interface ItemTouchHelperAdapter {
-
-    boolean onItemMove(int fromPosition, int toPosition);
-
-    void onItemDismiss(int position);
-
-}
