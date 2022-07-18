bbc7c29db9a3c1f069c616c104f084195d6f4df7
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-07-18 19:08:37 -0300

    Create the initial structure of Exercise Item compose

--- androidApp/src/main/java/br/com/sailboat/zerotohero/android/MainActivity.kt
+++ androidApp/src/main/java/br/com/sailboat/zerotohero/android/MainActivity.kt
@@ -3,13 +3,18 @@ package br.com.sailboat.zerotohero.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.sailboat.zerotohero.Greeting
 import br.com.sailboat.zerotohero.android.theme.MyComposeAppTheme
 
@@ -22,11 +27,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Exercises Screen")
+            Scaffold(
+                topBar = {
+                    TopAppBar {
+                        Text(
+                            text = "Exercises",
+                            modifier = Modifier.padding(16.dp),
+                        )
+                    }
                 }
+            ) {
+                ExerciseItem(
+                    name = "Kettlebell Swings",
+                    sets = 1,
+                    repetitions = 60,
+                )
             }
         }
     }
@@ -37,10 +52,27 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun ExerciseItem(name: String, sets: Int, repetitions: Int) {
+    Column(
+        modifier = Modifier.padding(
+            horizontal = 16.dp,
+            vertical = 8.dp,
+        )
+    ) {
+        Text(text = name)
+        Text(text = "$sets x $repetitions")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyComposeAppTheme {
-        Greeting("Android")
+        ExerciseItem(
+            name = "Kettlebell Swings",
+            sets = 1,
+            repetitions = 60,
+        )
     }
 }
